45425fc69337cf5fc4161ccb2f3f3e80888b88da
QAAutomation-lab <automationguide13@gmail.com>
2024-05-23 09:04:07 +0530

    JSON Example

---
+++ 17thJanApiSession/src/test/java/day1/jsonexamples/Example2.java
@@ -0,0 +1,32 @@
+package day1.jsonexamples;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+public class Example2 {
+	
+	@Test
+	public void testJsonArrayCreation() {
+		JSONObject obj1=new JSONObject();
+		obj1.put("Name", "Shailesh");
+		obj1.put("ID", 123);
+		obj1.put("Address", "Pune");
+		
+		JSONObject obj2=new JSONObject();
+		obj2.put("Name", "Aman");
+		obj2.put("ID", 456);
+		obj2.put("Address", "Mumbai");
+		
+		System.out.println("First JSON object: \n\t\t"+obj1);
+		System.out.println("Second JSON object: \n\t\t"+obj2);
+		
+		JSONArray jArray=new JSONArray();
+		jArray.add(obj1);
+		jArray.add(obj2);
+		System.out.println("JSON Array object: \n\t\t"+jArray);
+		for(Object obj:jArray) {
+			System.out.println(obj);
+		}
+	}
+}
